ea46a3b9b434b768351b0849805851d9f0e83dd6
Alicja <alanotowska@yahoo.co.uk>
2023-04-30 13:26:02 +0200

    added 3 pokemon objects to the pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [];
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']}, 
+    {name: 'Weedle', height: 0.3, type: ['bug', 'poison']}, 
+    {name: 'Pikachu', height: 0.4, type: 'electric'}
+];
