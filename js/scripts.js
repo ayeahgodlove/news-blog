5c2bf9752cb5e8b0130cdb7879ce2c61ba28b4e6
Alicja <alanotowska@yahoo.co.uk>
2023-05-02 16:26:58 +0200

    implements task 1.3 - for loop that iterates over each object

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ let pokemonList = [
     {name: 'Weedle', height: 0.3, type: ['bug', 'poison']}, 
     {name: 'Pikachu', height: 0.4, type: 'electric'}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i]
+    document.write(pokemon.name + " (height: " + pokemon.height + ")");
+    if (pokemon.height > 0.6) {
+        document.write(" - Wow, that\’s big!");
+    }
+    document.write("<br>");
+}
