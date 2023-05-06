379216bab50e4c61d3848d2ee30ece1434f3eeb7
Alicja <alanotowska@yahoo.co.uk>
2023-05-06 19:47:38 +0200

    wraps pokemonList in IIFE, adds public functions and validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']}, 
-    {name: 'Weedle', height: 0.3, type: ['bug', 'poison']}, 
-    {name: 'Pikachu', height: 0.4, type: 'electric'}
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+        {name: 'Weedle', height: 0.3, type: ['bug', 'poison']},
+        {name: 'Pikachu', height: 0.4, type: 'electric'}
+    ];
 
-pokemonList.forEach(function(pokemon) {
+    return {
+        add: function(item) {
+            if (typeof item === "object") {
+                let keys = Object.keys(item);
+                let hasName = keys.includes("name");
+                let hasHeight = keys.includes("height");
+                let hasType = keys.includes("type");
+                if (hasName && hasHeight && hasType) {
+                    pokemonList.push(item);
+                }
+            }
+        },
+
+        getAll: function() {
+            return pokemonList;
+        }
+    };
+})();
+
+pokemonRepository.getAll().forEach(printPokemon);
+
+function printPokemon(pokemon) {
     document.write(pokemon.name + " (height: " + pokemon.height + ")");
     if (pokemon.height > 0.6) {
         document.write(" - Wow, that\’s big!");
     }
     document.write("<br>");
-});
+}
